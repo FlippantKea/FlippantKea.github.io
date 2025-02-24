701d3e76eed492aafd84ec4270f84f3a6615d1f1
HannahTheDev <hannahsampsonnz@gmail.com>
2025-02-24 19:56:06 +1300

    Prove that we can have multiple pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Press from "./pages/Press";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Press" element={<Press />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/pages/Press.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Press() {
+  return (
+    <div>
+      <h1>Welcome to Tana's other page</h1>
+      <h2>It is much more boring yes</h2>
+    </div>
+  );
+}
+  
